8586a1865616a43d5b5a4462215860ee572cb657
v-wang <victorwang920@gmail.com>
2021-11-23 13:43:04 -0500

    added loadState to prevent page loading before fetch complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [repoList, setRepoList] = useState();
+  const [loadState, setLoadState] = useState(false);
+
+  useEffect(() => {
+    fetch('https://sheltered-sea-91500.herokuapp.com/github.com/trending')
+      .then(function (response) {
+        return response.text();
+      })
+      .then(function (html) {
+        // Convert the HTML string into a document object
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(html, 'text/html');
+
+        var repoUrlArray = [];
+        let repos = doc.querySelectorAll('.Box-row');
+
+        repos.forEach((repo) => {
+          // console.log(repo.querySelector('h1 > a'));
+          let repoUrl = repo.querySelector('h1 > a').getAttribute('href');
+          // console.log(`https://www.github.com${repoURL}`);
+          // console.log(`https://api.github.com/repos${repoURL}/issues`);
+          // console.log('###');
+          repoUrlArray.push(repoUrl);
+        });
+        setRepoList(repoUrlArray);
+        setLoadState(true);
+      })
+      .catch(function (err) {
+        console.warn('Error! Something went wrong.', err);
+      });
+  }, []);
+
+  if (loadState === false) {
+    return (
+      <div className='App'>
+        <h1>Loading...</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div className='App'>
+        <h1>{repoList[1]}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
