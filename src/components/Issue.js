17a77826be20f39296f746488526fe6cb58e58a9
v-wang <victorwang920@gmail.com>
2021-12-01 19:08:47 -0500

    truncate issue p's

--- src/components/Issue.js
+++ src/components/Issue.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Issue({ issue }) {
   return (
     <div className='issue'>
-      <h4>{issue.title}</h4>
+      <h2>{issue.title}</h2>
       <p>{issue.body === null ? 'No description provided.' : issue.body}</p>
     </div>
   );
--- src/components/RepoInfo.js
+++ src/components/RepoInfo.js
@@ -3,11 +3,17 @@ import React from 'react';
 function RepoInfo({ repository }) {
   return (
     <div className='repoGenInfo'>
-      <h5>{repository.name}</h5>
-      <h5>{repository.language}</h5>
-      <h5>{repository.owner.login}</h5>
-      <h5>{repository.html_url}</h5>
-      <h5>{repository.description}</h5>
+      <a href={repository.html_url} target='_blank' rel='noreferrer'>
+        <h1>{repository.name}</h1>
+      </a>
+      <h2>{repository.html_url}</h2>
+      <h2>Owner: {repository.owner.login}</h2>
+      <h2>
+        {repository.language === null
+          ? null
+          : `Language: ${repository.language}`}
+      </h2>
+      <p>{repository.description}</p>
     </div>
   );
 }
